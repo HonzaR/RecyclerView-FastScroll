ad7cc258cb91e713f46cc60b65b05171f24fe5c6
Honza Rychnovský <honzar@appsdevteam.com>
2018-09-10 13:22:58 +0200

    Correct the scroll bar position when adapter's items with different height.

--- sample/src/main/java/com/simplecityapps/recyclerview_fastscroll/sample/fragment/MultiViewTypeFragment.java
+++ sample/src/main/java/com/simplecityapps/recyclerview_fastscroll/sample/fragment/MultiViewTypeFragment.java
@@ -5,7 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,8 +22,26 @@ public class MultiViewTypeFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment, container, false);
 
         FastScrollRecyclerView recyclerView = rootView.findViewById(R.id.recycler);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(new MultiViewTypeAdapter());
+
+        final MultiViewTypeAdapter adapter = new MultiViewTypeAdapter();
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 2);
+        gridLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                switch(adapter.getItemViewType(position)){
+                    case R.layout.list_item:
+                        return 1;
+                    case R.layout.list_item_header:
+                        return 2;
+                    default:
+                        return 2;
+                }
+            }
+        });
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        recyclerView.setAdapter(adapter);
         DividerItemDecoration itemDecoration = new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL);
         itemDecoration.setDrawable(getResources().getDrawable(R.drawable.list_divider));
         recyclerView.addItemDecoration(itemDecoration);
